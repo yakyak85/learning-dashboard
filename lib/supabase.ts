768b39dab3592c28ee521467635f76a350180aec
yakyak85 <yusukeyako1130@gmail.com>
2025-04-22 17:03:26 +0900

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+if (!supabaseUrl || !supabaseAnonKey) {
+  console.warn("⚠️ Supabaseの環境変数が設定されていません。");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
